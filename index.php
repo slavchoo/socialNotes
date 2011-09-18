1b563e818b7c8c033cee96714f80c13b1bebd95b
slavchoo <slavchoo2@gmail.com>
2011-09-18 21:45:47 +0300

    base project structure

--- index.php
+++ index.php
@@ -1,57 +1,111 @@
-<html>
-    <head>
-        <title>Sticky Notes Demo</title>    
-        <link rel="stylesheet" href="/libs/jqnotes/css/ui-lightness/jquery-ui-1.8.12.custom.css" type="text/css">
-        <script type="text/javascript" src="/libs/jqnotes/script/jquery-1.3.2.min.js"></script>
-        <script type="text/javascript" src="/libs/jqnotes/script/jquery-ui-1.7.2.custom.min.js"></script>
-        <script type="text/javascript" src="/libs/jqnotes/script/jquery.stickynotes.js"></script>
-        <link rel="stylesheet" href="/libs/jqnotes/css/jquery.stickynotes.css" type="text/css">
+<?php
 
-    </head>
-    <body>
-        <h1>Sticky Notes Demo</h1>
-        <div id="notes" style="width:800px;height:500px;">
-        </div>
-        <script type="text/javascript" charset="utf-8">
-            var edited = function(note) {
-                alert("Edited note with id " + note.id + ", new text is: " + note.text);
-            }
-            var created = function(note) {
-                alert("Created note with id " + note.id + ", text is: " + note.text);
-            }
-			
-            var deleted = function(note) {
-                alert("Deleted note with id " + note.id + ", text is: " + note.text);
-            }
-			
-            var moved = function(note) {
-                alert("Moved note with id " + note.id + ", text is: " + note.text);
-            }	
-			
-            var resized = function(note) {
-                alert("Resized note with id " + note.id + ", text is: " + note.text);
-            }					
-		
-            jQuery(document).ready(function() {
-                var options = {
-                    notes:[{"id":1,
-                            "text":"Test Internet Explorer",
-                            "pos_x": 50,
-                            "pos_y": 50,	
-                            "width": 200,							
-                            "height": 200,													
-                        }]
-                    ,resizable: true
-                    ,controls: true 
-                    ,editCallback: edited
-                    ,createCallback: created
-                    ,deleteCallback: deleted
-                    ,moveCallback: moved					
-                    ,resizeCallback: resized					
-					
-                };
-                jQuery("#notes").stickyNotes(options);
-            });
-        </script>
-    </body>
-</html>
+/**
+ * The directory in which your application specific resources are located.
+ * The application directory must contain the bootstrap.php file.
+ *
+ * @see  http://kohanaframework.org/guide/about.install#application
+ */
+$application = 'application';
+
+/**
+ * The directory in which your modules are located.
+ *
+ * @see  http://kohanaframework.org/guide/about.install#modules
+ */
+$modules = 'modules';
+
+/**
+ * The directory in which the Kohana resources are located. The system
+ * directory must contain the classes/kohana.php file.
+ *
+ * @see  http://kohanaframework.org/guide/about.install#system
+ */
+$system = 'system';
+
+/**
+ * The default extension of resource files. If you change this, all resources
+ * must be renamed to use the new extension.
+ *
+ * @see  http://kohanaframework.org/guide/about.install#ext
+ */
+define('EXT', '.php');
+
+/**
+ * Set the PHP error reporting level. If you set this in php.ini, you remove this.
+ * @see  http://php.net/error_reporting
+ *
+ * When developing your application, it is highly recommended to enable notices
+ * and strict warnings. Enable them by using: E_ALL | E_STRICT
+ *
+ * In a production environment, it is safe to ignore notices and strict warnings.
+ * Disable them by using: E_ALL ^ E_NOTICE
+ *
+ * When using a legacy application with PHP >= 5.3, it is recommended to disable
+ * deprecated notices. Disable with: E_ALL & ~E_DEPRECATED
+ */
+error_reporting(E_ALL | E_STRICT);
+
+/**
+ * End of standard configuration! Changing any of the code below should only be
+ * attempted by those with a working knowledge of Kohana internals.
+ *
+ * @see  http://kohanaframework.org/guide/using.configuration
+ */
+
+// Set the full path to the docroot
+define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
+
+// Make the application relative to the docroot, for symlink'd index.php
+if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
+	$application = DOCROOT.$application;
+
+// Make the modules relative to the docroot, for symlink'd index.php
+if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
+	$modules = DOCROOT.$modules;
+
+// Make the system relative to the docroot, for symlink'd index.php
+if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
+	$system = DOCROOT.$system;
+
+// Define the absolute paths for configured directories
+define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
+define('MODPATH', realpath($modules).DIRECTORY_SEPARATOR);
+define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
+
+// Clean up the configuration vars
+unset($application, $modules, $system);
+
+if (file_exists('install'.EXT))
+{
+	// Load the installation check
+	return include 'install'.EXT;
+}
+
+/**
+ * Define the start time of the application, used for profiling.
+ */
+if ( ! defined('KOHANA_START_TIME'))
+{
+	define('KOHANA_START_TIME', microtime(TRUE));
+}
+
+/**
+ * Define the memory usage at the start of the application, used for profiling.
+ */
+if ( ! defined('KOHANA_START_MEMORY'))
+{
+	define('KOHANA_START_MEMORY', memory_get_usage());
+}
+
+// Bootstrap the application
+require APPPATH.'bootstrap'.EXT;
+
+/**
+ * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
+ * If no source is specified, the URI will be automatically detected.
+ */
+echo Request::factory()
+	->execute()
+	->send_headers()
+	->body();
